b733fc6fae53cfd396dec0c745724b8e91fc5fed
akshay / wildfire <akshay.wildfire@pm.me>
2020-11-28 15:17:28 +0530

    init, same as 404

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+include_once ('../../../tribe.init.php');
+include_once (__DIR__.'/header.php');
+
+if (!$_SESSION['user']['id']) {header ('Location: /user/login'); die();}
+
+if (($types['webapp']['user_theme']??false) && file_exists(THEME_PATH.'/user-'.$slug.'.php')):
+	include_once (THEME_PATH.'/user-'.$slug.'.php');
+else:
+	include_once (THEME_PATH.'/404.php');
+endif; ?>
+
+<?php include_once (__DIR__.'/footer.php'); ?>
