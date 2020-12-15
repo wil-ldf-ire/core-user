fef270b6e8533ee68758b8292179785aadf4caed
akshay / jigmet <akshay.wildfire@pm.me>
2020-12-16 02:21:01 +0530

    cleanup

--- login.php
+++ login.php
@@ -5,10 +5,10 @@ if ($_GET['action'] == 'exit') {
 	header('Location: ' . BASE_URL . '/user/login');
 }
 
-if (($_POST['email'] || $_POST['mobile']) && $_POST['password']) {
-	if ($_POST['email']) {
+if ((($_POST['email'] ?? false) || ($_POST['mobile'] ?? false)) && ($_POST['password'] ?? false)) {
+	if (($_POST['email'] ?? false)) {
 		$q = $sql->executeSQL("SELECT `id` FROM `data` WHERE `content`->'$.email'='" . $_POST['email'] . "' && `content`->'$.password'='" . md5($_POST['password']) . "' && `content`->'$.type'='user'");
-	} else if ($_POST['mobile']) {
+	} else if (($_POST['mobile'] ?? false)) {
 		$q = $sql->executeSQL("SELECT `id` FROM `data` WHERE `content`->'$.mobile'='" . $_POST['mobile'] . "' && `content`->'$.password'='" . md5($_POST['password']) . "' && `content`->'$.type'='user'");
 	}
 
@@ -16,7 +16,7 @@ if (($_POST['email'] || $_POST['mobile']) && $_POST['password']) {
 		$user = $dash->get_content($q[0]['id']);
 		$dash->after_login($user['role_slug'], (isset($_POST['redirect_url']) ? $_POST['redirect_url'] : ''));
 	}
-} elseif ($_SESSION['user']['id']) {
+} elseif (($_SESSION['user']['id'] ?? false)) {
 	$user = $dash->get_content($_SESSION['user']['id']);
 	$dash->after_login($user['role_slug'], (isset($_POST['redirect_url']) ? $_POST['redirect_url'] : ''));
 }
@@ -36,19 +36,19 @@ elseif (
 else:
 ?>
 
-<form class="form-user" method="post" action="/user/login"><h2><?php echo $menus['main']['logo']['name']; ?></h2>
+<form class="form-user" method="post" action="/user/login"><h2><?php echo ($menus['main']['logo']['name'] ?? ''); ?></h2>
 	<h4 class="my-3 font-weight-normal"><span class="fas fa-lock"></span>&nbsp;Sign in</h4>
 	<?=($_POST ? '<div class="alert alert-danger">Wrong credentials. Please try again.</div>' : '');?>
 	<label for="inputEmail" class="sr-only">Email address</label>
-	<input type="email" name="email" id="inputEmail" class="form-control my-1" value="<?=$_POST['email'];?>" placeholder="Email address" required autofocus>
+	<input type="email" name="email" id="inputEmail" class="form-control my-1" value="<?=($_POST['email'] ?? false);?>" placeholder="Email address" required autofocus>
 	<label for="inputPassword" class="sr-only">Password</label>
 	<input type="password" name="password" id="inputPassword" class="form-control my-1" placeholder="Password" required>
 	<div class="checkbox my-1 small"><label><input type="checkbox" class="my-0" value="remember-me"> Remember me</label></div>
 	<button type="submit" class="btn btn-sm btn-primary btn-block my-1">Sign in</button>
 	<a class="btn btn-sm btn-outline-primary btn-block my-1" href="/user/register">Register</a>
 	<p class="text-muted small my-2"><a href="/user/forgot-password"><span class="fas fa-key"></span>&nbsp;Forgot password?</a></p>
-	<p class="text-muted small my-5"><?php echo '<a href="' . BASE_URL . '"><span class="fas fa-angle-double-left"></span>&nbsp;' . $menus['main']['logo']['name'] . '</a>'; ?></p>
-	<p class="text-muted small my-5">&copy; <?php echo (date('Y') == '2020' ? date('Y') : '2020 - ' . date('Y')); ?> Wildfire</p>
+	<p class="text-muted small my-5"><?php echo '<a href="' . BASE_URL . '"><span class="fas fa-angle-double-left"></span>&nbsp;' . ($menus['main']['logo']['name'] ?? '') . '</a>'; ?></p>
+	<p class="text-muted small my-5">&copy; <?php echo (date('Y') == '2020' ? date('Y') : '2020 - ' . date('Y')) . ($menus['main']['logo']['name'] ?? 'Wildfire'); ?></p>
 </form>
 
 <?php endif;?>
--- register.php
+++ register.php
@@ -5,12 +5,12 @@ $types = $dash->getTypes();
 
 $error_op = '';
 
-if ($_SESSION['user']['id']) {
+if (($_SESSION['user']['id'] ?? false)) {
 	$user = $dash->get_content($_SESSION['user']['id']);
 	$dash->after_login($user['role_slug'], isset($_POST['redirect_url']) ? $_POST['redirect_url'] : '');
 } elseif (
-	($_POST['email'] || $_POST['mobile']) &&
-	$_POST['password'] &&
+	(($_POST['email'] ?? false) || ($_POST['mobile'] ?? false)) &&
+	($_POST['password'] ?? false) &&
 	($_POST['password'] == $_POST['confirm_password'])
 ) {
 	if ($_POST['email']) {
@@ -47,14 +47,14 @@ else:
 <?=$error_op ?? ''?>
 
 <form class="form-user" method="post" action="/user/register">
-	<h2><?=$menus['main']['logo']['name']?></h2>
+	<h2><?=($menus['main']['logo']['name'] ?? '')?></h2>
 	<h4 class="my-3 font-weight-normal">
 		<span class="fas fa-lock"></span>&nbsp;Register
 	</h4>
 
 <?php
 $type = 'user';
-$role = $types['user']['roles'][$_GET[role]];
+$role = ($types['user']['roles'][$_GET[role]] ?? false);
 if ($role['slug']):
 ?>
     <input type="hidden" name="role_slug" value="<?=$role['slug']?>">
@@ -78,10 +78,10 @@ if ($role['slug']):
 	</p>
 	<p class="text-muted small my-5">
 		<a href="<?=BASE_URL?>">
-			<span class="fas fa-angle-double-left"></span>&nbsp;<?=$menus['main']['logo']['name']?>
+			<span class="fas fa-angle-double-left"></span>&nbsp;<?=($menus['main']['logo']['name'] ?? '')?>
 		</a>
 	</p>
-	<p class="text-muted small my-5">&copy;<?=date('Y') == '2020' ? date('Y') : '2020 - ' . date('Y')?> Wildfire</p>
+	<p class="text-muted small my-5">&copy; <?php echo (date('Y') == '2020' ? date('Y') : '2020 - ' . date('Y')) . ($menus['main']['logo']['name'] ?? 'Wildfire'); ?></p>
 </form>
 
 <?php endif?>
