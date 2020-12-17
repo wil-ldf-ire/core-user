ec3b3b71ade7b5cc6e6f40867b8948ff5e4928fe
Apurv Jyotirmay <apjyotirmay@gmail.com>
2020-12-17 23:11:36 +0530

    added logic to handle type user without slug

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ . '/header.php';
+include_once __DIR__.'/header.php';
 
 if (!$_SESSION['user']['id']) {
     header('Location: /user/login');
@@ -10,15 +10,23 @@ $dash = new Wildfire\Core\Dash;
 $types = $dash->getTypes();
 
 if ($types['webapp']['user_theme'] ?? false) {
-    if (file_exists(THEME_PATH . '/pages/user/' . $slug . '.php')) {
-        include_once THEME_PATH . '/pages/user/' . $slug . '.php';
-    } elseif (file_exists(THEME_PATH . '/user-' . $slug . '.php')) {
-        include_once THEME_PATH . '/user-' . $slug . '.php';
+    if (!$slug) {
+        if (file_exists(__DIR__.'/index.php')) {
+            include_once __DIR__.'/index.php';
+        } else {
+            die('file not found');
+        }
     } else {
-        die('file not found');
+        if (file_exists(THEME_PATH.'/pages/user/'.$slug.'.php')) {
+            include_once THEME_PATH.'/pages/user/'.$slug.'.php';
+        } elseif (file_exists(THEME_PATH.'/user-'.$slug.'.php')) {
+            include_once THEME_PATH.'/user-'.$slug.'.php';
+        } else {
+            die('file not found');
+        }
     }
 } else {
-    include_once THEME_PATH . '/404.php';
+    include_once THEME_PATH.'/errors/404.php';
 }
 
-include_once __DIR__ . '/footer.php';
+include_once __DIR__.'/footer.php';
