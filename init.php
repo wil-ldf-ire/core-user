3f0515af799d357b481950b3ce2f6a67b9723c59
akshay / wildfire <akshay.wildfire@pm.me>
2020-12-21 13:28:19 +0530

    user path option for theme folder

--- init.php
+++ init.php
@@ -1,32 +1,34 @@
 <?php
-include_once __DIR__.'/header.php';
+include_once __DIR__ . '/header.php';
 
 if (!$_SESSION['user']['id']) {
-    header('Location: /user/login');
-    die();
+	header('Location: /user/login');
+	die();
 }
 
 $dash = new Wildfire\Core\Dash;
 $types = $dash->getTypes();
 
 if ($types['webapp']['user_theme'] ?? false) {
-    if (!$slug) {
-        if (file_exists(__DIR__.'/index.php')) {
-            include_once __DIR__.'/index.php';
-        } else {
-            die('file not found');
-        }
-    } else {
-        if (file_exists(THEME_PATH.'/pages/user/'.$slug.'.php')) {
-            include_once THEME_PATH.'/pages/user/'.$slug.'.php';
-        } elseif (file_exists(THEME_PATH.'/user-'.$slug.'.php')) {
-            include_once THEME_PATH.'/user-'.$slug.'.php';
-        } else {
-            die('file not found');
-        }
-    }
+	if (!$slug) {
+		if (file_exists(__DIR__ . '/index.php')) {
+			include_once __DIR__ . '/index.php';
+		} else {
+			die('file not found');
+		}
+	} else {
+		if (file_exists(THEME_PATH . '/pages/user/' . $slug . '.php')) {
+			include_once THEME_PATH . '/pages/user/' . $slug . '.php';
+		} elseif (file_exists(THEME_PATH . '/pages/user-' . $slug . '.php')) {
+			include_once THEME_PATH . '/pages/user-' . $slug . '.php';
+		} elseif (file_exists(THEME_PATH . '/user-' . $slug . '.php')) {
+			include_once THEME_PATH . '/user-' . $slug . '.php';
+		} else {
+			die('file not found');
+		}
+	}
 } else {
-    include_once THEME_PATH.'/errors/404.php';
+	include_once THEME_PATH . '/errors/404.php';
 }
 
-include_once __DIR__.'/footer.php';
+include_once __DIR__ . '/footer.php';
