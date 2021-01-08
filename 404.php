d829f8b9510cc618d973a55221cc3383842c82e6
akshay / wildfire <akshay.wildfire@pm.me>
2021-01-08 17:59:13 +0530

    redirect to 404, if file not available in auth dir

--- 404.php
+++ 404.php
@@ -1,25 +1,25 @@
 <?php
 
-include_once('../../../tribe.init.php');
-include_once(__DIR__.'/header.php');
+include_once '../../../tribe.init.php';
+include_once __DIR__ . '/header.php';
 
 if (!$_SESSION['user']['id']) {
-    header('Location: /user/login');
-    die();
+	header('Location: /user/login');
+	die();
 }
 
 if (
-    ($types['webapp']['user_theme'] ?? false) &&
-    file_exists(THEME_PATH.'/pages/user/'.$slug.'.php')
+	($types['webapp']['user_theme'] ?? false) &&
+	file_exists(THEME_PATH . '/pages/user/' . $slug . '.php')
 ):
-    include_once THEME_PATH.'/pages/user/'.$slug.'.php';
+	include_once THEME_PATH . '/pages/user/' . $slug . '.php';
 elseif (
-    ($types['webapp']['user_theme'] ?? false) &&
-    file_exists(THEME_PATH.'/user-'.$slug.'.php')
+	($types['webapp']['user_theme'] ?? false) &&
+	file_exists(THEME_PATH . '/user-' . $slug . '.php')
 ):
-    include_once THEME_PATH.'/user-'.$slug.'.php';
+	include_once THEME_PATH . '/user-' . $slug . '.php';
 else:
-    include_once(THEME_PATH.'/404.php');
+	include_once THEME_PATH . '/404.php';
 endif;
 
-include_once __DIR__.'/footer.php';
+include_once __DIR__ . '/footer.php';
