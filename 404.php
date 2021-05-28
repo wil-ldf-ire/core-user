cc232a2f71c365fdf743e8418bdb65a63dc2fd16
akshay / tw <akshay.wildfire@pm.me>
2021-05-28 18:14:19 +0530

    currentUser instead of session user

--- 404.php
+++ 404.php
@@ -2,23 +2,23 @@
 include_once __DIR__ . '/init.php';
 include_once __DIR__ . '/includes/_header.php';
 
-if (!$_SESSION['user']['id']) {
-	header('Location: /user/login');
-	die();
+if (!$currentUser['id']) {
+    header('Location: /user/login');
+    die();
 }
 
 if (
-	($types['webapp']['user_theme'] ?? false) &&
-	file_exists(THEME_PATH . '/pages/user/' . $slug . '.php')
+    ($types['webapp']['user_theme'] ?? false) &&
+    file_exists(THEME_PATH . '/pages/user/' . $slug . '.php')
 ):
-	include_once THEME_PATH . '/pages/user/' . $slug . '.php';
+    include_once THEME_PATH . '/pages/user/' . $slug . '.php';
 elseif (
-	($types['webapp']['user_theme'] ?? false) &&
-	file_exists(THEME_PATH . '/user-' . $slug . '.php')
+    ($types['webapp']['user_theme'] ?? false) &&
+    file_exists(THEME_PATH . '/user-' . $slug . '.php')
 ):
-	include_once THEME_PATH . '/user-' . $slug . '.php';
+    include_once THEME_PATH . '/user-' . $slug . '.php';
 else:
-	include_once THEME_PATH . '/404.php';
+    include_once THEME_PATH . '/404.php';
 endif;
 
-include_once __DIR__.'/includes/_footer.php';
+include_once __DIR__ . '/includes/_footer.php';
