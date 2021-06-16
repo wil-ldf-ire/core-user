ffb20f461310787c13c10eb478e750a3b9f4e1d1
Apurv Jyotirmay <mail@jyotirmay.dev>
2021-06-16 12:26:12 +0530

    user theme override doesn't include admin header

--- login.php
+++ login.php
@@ -27,93 +27,92 @@ if (($form_email || $form_mobile) && $form_password) {
     $auth->doAfterLogin($user, $redirect_url);
 }
 
-require_once __DIR__.'/includes/_header.php';
 
 $use_custom_theme = $types['webapp']['user_theme'] ?? false;
 
 if ($use_custom_theme && file_exists(THEME_PATH.'/pages/user/login.php')):
     require_once THEME_PATH.'/pages/user/login.php';
-elseif ($use_custom_theme && file_exists(THEME_PATH.'/user-login.php')):
-    require_once THEME_PATH.'/user-login.php';
-else:
+    elseif ($use_custom_theme && file_exists(THEME_PATH.'/user-login.php')):
+        require_once THEME_PATH.'/user-login.php';
+    else: // if custom user theme doesn't exitst
+        require_once __DIR__.'/includes/_header.php';
 ?>
 
-<form class="form-user" method="post" action="/user/login">
-    <h2>
-        <?= $menus['main']['logo']['name'] ?? '' ?>
-    </h2>
+    <form class="form-user" method="post" action="/user/login">
+        <h2>
+            <?= $menus['main']['logo']['name'] ?? '' ?>
+        </h2>
 
-	<h4 class="my-3 font-weight-normal">
-        <span class="fas fa-lock"></span>&nbsp;Sign in
-    </h4>
+        <h4 class="my-3 font-weight-normal">
+            <span class="fas fa-lock"></span>&nbsp;Sign in
+        </h4>
 
-    <?php if ($_POST): ?>
-        <div class="alert alert-danger"
-            >Wrong credentials. Please try again.
+        <?php if ($_POST): ?>
+            <div class="alert alert-danger"
+                >Wrong credentials. Please try again.
+            </div>
+        <?php endif; ?>
+
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input
+            type="email"
+            name="email"
+            id="inputEmail"
+            class="form-control my-1"
+            value="<?= $form_email ?>"
+            placeholder="Email address"
+            required
+            autofocus
+        >
+
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input
+            type="password"
+            name="password"
+            id="inputPassword"
+            class="form-control my-1"
+            placeholder="Password"
+            required
+        >
+
+        <div class="checkbox my-1 small">
+            <label>
+                <input type="checkbox" class="my-0" value="remember-me"> Remember me
+            </label>
         </div>
-    <?php endif; ?>
 
-	<label for="inputEmail" class="sr-only">Email address</label>
-	<input
-        type="email"
-        name="email"
-        id="inputEmail"
-        class="form-control my-1"
-        value="<?= $form_email ?>"
-        placeholder="Email address"
-        required
-        autofocus
-    >
+        <button type="submit" class="btn btn-sm btn-primary btn-block my-1"
+            >Sign in
+        </button>
 
-	<label for="inputPassword" class="sr-only">Password</label>
-	<input
-        type="password"
-        name="password"
-        id="inputPassword"
-        class="form-control my-1"
-        placeholder="Password"
-        required
-    >
-
-	<div class="checkbox my-1 small">
-        <label>
-            <input type="checkbox" class="my-0" value="remember-me"> Remember me
-        </label>
-    </div>
-
-	<button type="submit" class="btn btn-sm btn-primary btn-block my-1"
-        >Sign in
-    </button>
-
-	<a
-        class="btn btn-sm btn-outline-primary btn-block my-1"
-        href="/user/register"
-        >Register
-    </a>
-
-	<p class="text-muted small my-2">
-        <a href="/user/forgot-password">
-            <span class="fas fa-key"></span> Forgot password?
+        <a
+            class="btn btn-sm btn-outline-primary btn-block my-1"
+            href="/user/register"
+            >Register
         </a>
-    </p>
 
-	<p class="text-muted small my-5">
-        <a href="/">
-            <span class="fas fa-angle-double-left"
-                >&nbsp;<?= $menus['main']['logo']['name'] ?? '' ?>
-            </span>
-        </a>
-    </p>
+        <p class="text-muted small my-2">
+            <a href="/user/forgot-password">
+                <span class="fas fa-key"></span> Forgot password?
+            </a>
+        </p>
 
-	<p class="text-muted small my-5">
-        &copy;&nbsp;
-        <?= date('Y') == '2020 ' ? date('Y') : '2020 - ' . date('Y') ?>&nbsp;
-        <?= $menus['main']['logo']['name'] ?? 'Wildfire' ?>
-    </p>
-</form>
+        <p class="text-muted small my-5">
+            <a href="/">
+                <span class="fas fa-angle-double-left"
+                    >&nbsp;<?= $menus['main']['logo']['name'] ?? '' ?>
+                </span>
+            </a>
+        </p>
+
+        <p class="text-muted small my-5">
+            &copy;&nbsp;
+            <?= date('Y') == '2020 ' ? date('Y') : '2020 - ' . date('Y') ?>&nbsp;
+            <?= $menus['main']['logo']['name'] ?? 'Wildfire' ?>
+        </p>
+    </form>
 
 <?php
-endif;
-
-require_once __DIR__.'/includes/_footer.php';
+    require_once __DIR__.'/includes/_footer.php';
+endif; // custom theme override
 ?>
--- register.php
+++ register.php
@@ -31,8 +31,6 @@ if ($currentUser['id'] ?? false) {
     $error_op = true;
 }
 
-include_once __DIR__ . '/includes/_header.php';
-
 $use_custom_theme = $types['webapp']['user_theme'] ?? false;
 
 if ($use_custom_theme && file_exists(THEME_PATH.'/pages/user/register.php')):
@@ -40,71 +38,72 @@ if ($use_custom_theme && file_exists(THEME_PATH.'/pages/user/register.php')):
 elseif ($use_custom_theme && file_exists(THEME_PATH.'/user-register.php')):
     include_once THEME_PATH.'/user-register.php';
 else:
+	// if custom user theme pages don't exist
+	include_once __DIR__ . '/includes/_header.php';
 ?>
-<?php if ($error_op): ?>
-	<div class="alert alert-danger"
-		>Form not submitted. Please try again.
-	</div>
-<?php endif ?>
+	<?php if ($error_op): ?>
+		<div class="alert alert-danger"
+			>Form not submitted. Please try again.
+		</div>
+	<?php endif ?>
 
-<form class="form-user" method="post" action="/user/register">
-	<h2><?= $app_title ?></h2>
-	<h4 class="my-3 font-weight-normal">
-		<span class="fas fa-lock"></span> Register
-	</h4>
+	<form class="form-user" method="post" action="/user/register">
+		<h2><?= $app_title ?></h2>
+		<h4 class="my-3 font-weight-normal">
+			<span class="fas fa-lock"></span> Register
+		</h4>
+
+		<?php
+			$type = 'user';
+			if (isset($_GET['role'])) {
+				$role = $types['user']['roles'][$_GET['role']] ?? false;
+			} else {
+				$role['slug'] = 'user'; // default user role as unprivileged user
+			}
+
+			if ($role['slug']):
+		?>
+		<input type="hidden" name="role_slug" value="<?=$role['slug']?>">
+		<?php endif?>
+
+		<?php include __DIR__ . '/../admin/form.php'?>
+
+		<div class="checkbox my-1 small">
+			<label>
+				<input type="checkbox" class="my-0" value="remember-me">
+				I agree with the terms and conditions
+			</label>
+		</div>
+
+		<button type="submit" class="btn btn-sm btn-primary btn-block my-1"
+			>Register
+		</button>
+
+		<a href="/user/login" class="btn btn-sm btn-outline-primary btn-block my-1"
+			>Sign in
+		</a>
+
+		<p class="text-muted small my-2">
+			<a href="/user/forgot-password">
+				<span class="fas fa-key"></span> Forgot password?
+			</a>
+		</p>
+
+		<p class="text-muted small my-5">
+			<a href="/">
+				<span class="fas fa-angle-double-left"></span> <?= $app_title ?>
+			</a>
+		</p>
+
+		<p class="text-muted small my-5">
+			&copy;&nbsp;
+			<?php $year = date('Y') ?>
+			<?= $year == '2020 ' ? $year : "2020 - $year" ?>&nbsp;
+			<?= $app_title ?? 'Wildfire' ?>
+		</p>
+	</form>
 
 <?php
-$type = 'user';
-if (isset($_GET['role'])) {
-	$role = $types['user']['roles'][$_GET['role']] ?? false;
-} else {
-	$role['slug'] = 'user'; // default user role as unprivileged user
-}
-
-if ($role['slug']):
-?>
-    <input type="hidden" name="role_slug" value="<?=$role['slug']?>">
-<?php endif?>
-
-<?php include __DIR__ . '/../admin/form.php'?>
-
-	<div class="checkbox my-1 small">
-		<label>
-			<input type="checkbox" class="my-0" value="remember-me">
-			I agree with the terms and conditions
-		</label>
-	</div>
-
-	<button type="submit" class="btn btn-sm btn-primary btn-block my-1"
-		>Register
-	</button>
-
-	<a href="/user/login" class="btn btn-sm btn-outline-primary btn-block my-1"
-		>Sign in
-	</a>
-
-	<p class="text-muted small my-2">
-		<a href="/user/forgot-password">
-			<span class="fas fa-key"></span> Forgot password?
-		</a>
-	</p>
-
-	<p class="text-muted small my-5">
-		<a href="/">
-			<span class="fas fa-angle-double-left"></span> <?= $app_title ?>
-		</a>
-	</p>
-
-	<p class="text-muted small my-5">
-		&copy;&nbsp;
-		<?php $year = date('Y') ?>
-		<?= $year == '2020 ' ? $year : "2020 - $year" ?>&nbsp;
-		<?= $app_title ?? 'Wildfire' ?>
-	</p>
-</form>
-
-<?php
-endif;
-
-include_once __DIR__.'/includes/_footer.php';
+	include_once __DIR__.'/includes/_footer.php';
+endif; // custom user theme
 ?>
