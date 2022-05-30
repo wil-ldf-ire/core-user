6cb9f1f4746a815fbbe9ad20eac8d36d69194ac0
Apurv Jyotirmay <mail@jyotirmay.dev>
2022-05-30 15:32:45 +0530

    patch: do not register duplicate accounts

--- src/Auth.php
+++ src/Auth.php
@@ -5,219 +5,243 @@ use \Wildfire\Core\Dash;
 use \Wildfire\Core\MySQL;
 use \Firebase\JWT\JWT;
 
-class Auth {
+class Auth
+{
+    protected static $types;
+    public $cookie_options;
 
-	protected static $types;
-	public $cookie_options;
+    public function __construct()
+    {
+        $dash = new Dash();
+        self::$types = $dash->get_types(ABSOLUTE_PATH . '/config/types.json');
+        $_secure = ($_ENV['SSL'] == 'true');
 
-	public function __construct() {
-		$dash = new Dash();
-		self::$types = $dash->get_types(ABSOLUTE_PATH . '/config/types.json');
-		$_secure = ($_ENV['SSL'] == 'true');
+        $this->cookie_options = [
+            'expires' => 'Session',
+            'path' => '/',
+            'secure' => $_secure,
+            'httponly' => true,
+            'samesite' => 'Lax'
+        ];
+    }
 
-		$this->cookie_options = [
-			'expires' => 'Session',
-			'path' => '/',
-			'secure' => $_secure,
-			'httponly' => true,
-			'samesite' => 'Lax'
-		];
-	}
+    public function doAfterLogin($user, $redirect_url = '', $remember = false)
+    {
+        global $_SESSION;
 
-	public function doAfterLogin($user, $redirect_url = '', $remember = false) {
-		global $_SESSION;
+        $roleslug = $user['role_slug'];
+        $types = self::$types;
+        $user['role'] = $types['user']['roles'][$roleslug]['role'];
 
-		$roleslug = $user['role_slug'];
-		$types = self::$types;
-		$user['role'] = $types['user']['roles'][$roleslug]['role'];
+        //for admin and crew (staff)
+        if ($user['role'] == 'admin' || $user['role'] == 'crew') {
+            $user['wildfire_dashboard_access'] = 1;
+            $token = $this->setCurrentUser($user);
 
-		//for admin and crew (staff)
-		if ($user['role'] == 'admin' || $user['role'] == 'crew') {
-			$user['wildfire_dashboard_access'] = 1;
-			$token = $this->setCurrentUser($user);
+            $_redirect = 'Location: ' . (trim($redirect_url) ?: '/admin');
+        }
 
-			$_redirect = 'Location: ' . (trim($redirect_url) ?: '/admin');
-		}
+        //for members
+        elseif ($user['role'] == 'member') {
+            $user['wildfire_dashboard_access'] = 0;
+            $token = $this->setCurrentUser($user);
 
-		//for members
-		elseif ($user['role'] == 'member') {
-			$user['wildfire_dashboard_access'] = 0;
-			$token = $this->setCurrentUser($user);
+            $_redirect = 'Location: ' . (trim($redirect_url) ?: '/user');
+        }
 
-			$_redirect = 'Location: ' . (trim($redirect_url) ?: '/user');
-		}
+        //for visitors and anybody else
+        else {
+            $_redirect = 'Location: ' . (trim($redirect_url) ?: '/');
+        }
 
-		//for visitors and anybody else
-		else {
-			$_redirect = 'Location: ' . (trim($redirect_url) ?: '/');
-		}
+        $access_token = "{$token['token_type']} {$token['access_token']}";
 
-		$access_token = "{$token['token_type']} {$token['access_token']}";
+        // setting http cookie
+        $cookie_options = $this->cookie_options;
+        if ($remember) {
+            $cookie_options['expires'] = strtotime('+45 days');
+            setcookie('access_token', $access_token, $cookie_options);
+        } else {
+            setcookie('access_token', $access_token, $cookie_options);
+        }
 
-		// setting http cookie
-		$cookie_options = $this->cookie_options;
-		if ($remember) {
-			$cookie_options['expires'] = strtotime('+45 days');
-			setcookie('access_token', $access_token, $cookie_options);
-		} else {
-			setcookie('access_token', $access_token, $cookie_options);
-		}
+        ob_start();
+        header($_redirect);
+        ob_end_flush();
+    }
 
-		ob_start();
-		header($_redirect);
-		ob_end_flush();
-	}
+    public function getApiAccess($api_key, $api_secret)
+    {
+        $sql = new MySQL();
+        $types = self::$types;
 
-	public function getApiAccess($api_key, $api_secret) {
-		$sql = new MySQL();
-		$types = self::$types;
-
-		$user_id = (string) json_decode($sql->executeSQL("
+        $user_id = (string) json_decode($sql->executeSQL("
             SELECT `user_id` FROM `data`
             WHERE
             `content`->'$.api_key'='" . $api_key . "' &&
             `content`->'$.api_secret'='" . $api_secret . "' &&
             `type`='api_key_secret' LIMIT 0,1")[0]['user_id'], true);
 
-		$user = $this->getUser($user_id);
-		$roleslug = $user['role_slug'];
-		$user['role'] = $types['user']['roles'][$roleslug]['role'];
+        $user = $this->getUser($user_id);
+        $roleslug = $user['role_slug'];
+        $user['role'] = $types['user']['roles'][$roleslug]['role'];
 
-		//for admin and dev (staff)
-		if ($types['user']['roles'][$roleslug]['role'] == 'admin' || $types['user']['roles'][$roleslug]['role'] == 'dev') {
-			return $this->setCurrentUser($user, 900);
-		} else {
-			return false;
-		}
+        //for admin and dev (staff)
+        if ($types['user']['roles'][$roleslug]['role'] == 'admin' || $types['user']['roles'][$roleslug]['role'] == 'dev') {
+            return $this->setCurrentUser($user, 900);
+        } else {
+            return false;
+        }
+    }
 
-	}
+    public function getUser($val)
+    {
+        $sql = new MySQL();
+        $or = array();
 
-	public function getUser($val) {
-		$sql = new MySQL();
-		$or = array();
+        if (is_int($val)) {
+            $q = $sql->executeSQL("SELECT * FROM `data` WHERE `id`='$val' && `type`='user' ORDER BY `id` DESC LIMIT 0,1");
+        } else {
+            $q = $sql->executeSQL("SELECT * FROM `data` WHERE `user_id`='$val' && `type`='user' ORDER BY `id` DESC LIMIT 0,1");
+        }
 
-		if (is_int($val)) {
-			$q = $sql->executeSQL("SELECT * FROM `data` WHERE `id`='$val' && `type`='user' ORDER BY `id` DESC LIMIT 0,1");
-		} else {
-			$q = $sql->executeSQL("SELECT * FROM `data` WHERE `user_id`='$val' && `type`='user' ORDER BY `id` DESC LIMIT 0,1");
-		}
+        if ($q[0]['id']) {
+            $or = json_decode($q[0]['content'], true);
+            $or['id'] = $q[0]['id'];
+            $or['updated_on'] = $q[0]['updated_on'];
+            $or['created_on'] = $q[0]['created_on'];
+            return $or;
+        } else {
+            return 0;
+        }
+    }
 
-		if ($q[0]['id']) {
-			$or = json_decode($q[0]['content'], true);
-			$or['id'] = $q[0]['id'];
-			$or['updated_on'] = $q[0]['updated_on'];
-			$or['created_on'] = $q[0]['created_on'];
-			return $or;
-		} else {
-			return 0;
-		}
+    public function getCurrentUser($access_token = '')
+    {
+        global $_SESSION, $_ENV;
 
-	}
+        $token = $_COOKIE['access_token'] ?? '';
+        $token = trim($token);
+        if (!$token) {
+            return false;
+        }
 
-	public function getCurrentUser($access_token = '') {
-		global $_SESSION, $_ENV;
+        $token = str_replace('Bearer ', '', $token);
 
-		$token = $_COOKIE['access_token'] ?? '';
-		$token = trim($token);
-		if (!$token) return false;
+        try {
+            $_jwt_secret = $_ENV['TRIBE_API_SECRET_KEY'] ?? $_ENV['DB_PASS'];
+            $decoded = (array) JWT::decode($token, $_jwt_secret, ['HS256']);
 
-		$token = str_replace('Bearer ', '', $token);
+            if (isset($_SESSION['user_id'])) {
+                return ($_SESSION['user_id'] == $decoded['user_id']) ? $_SESSION : false;
+            } else {
+                $dash = new Dash;
+                $user = $dash->getObject(['type' => 'user', 'slug' => strtolower($decoded['user_id'])]);
+                $_SESSION = $user;
 
-		try {
-			$_jwt_secret = $_ENV['TRIBE_API_SECRET_KEY'] ?? $_ENV['DB_PASS'];
-			$decoded = (array) JWT::decode($token, $_jwt_secret, ['HS256']);
+                return $_SESSION;
+            }
+        } catch (\Exception $e) {
+            return ($e->getMessage() == "Expired token") ? 'expired' : false;
+        }
+    }
 
-			if (isset($_SESSION['user_id'])) {
-				return ($_SESSION['user_id'] == $decoded['user_id']) ? $_SESSION : false;
-			} else {
-				$dash = new Dash;
-				$user = $dash->getObject(['type' => 'user', 'slug' => strtolower($decoded['user_id'])]);
-				$_SESSION = $user;
+    public function setCurrentUser($user, $timeout = 0)
+    {
+        global $_SESSION, $_ENV;
 
-				return $_SESSION;
-			}
-		} catch (\Exception $e) {
-			return ($e->getMessage() == "Expired token") ? 'expired' : false;
-		}
+        $payload = array(
+            "iss" => $_ENV['BASE_URL'] ?? $_ENV['WEB_URL'] ?? null, //“iss” (Issuer) Claim
+            "aud" => $_ENV['BASE_URL'] ?? $_ENV['WEB_URL'] ?? null,
+            "iat" => time(), //"iat" (Issued At) Claim
+            "nbf" => time(), //"nbf" (Not Before) Claim
+        );
 
-	}
+        $_user = [
+            "name" => $user['name'] ?? null,
+            "user_id" => $user['user_id']
+        ];
 
-	public function setCurrentUser($user, $timeout = 0) {
-		global $_SESSION, $_ENV;
+        // "exp" (Expiration Time) Claim
+        if ($timeout) {
+            $payload["exp"] = time() + $timeout;
+        }
 
-		$payload = array(
-			"iss" => $_ENV['BASE_URL'] ?? $_ENV['WEB_URL'] ?? null, //“iss” (Issuer) Claim
-			"aud" => $_ENV['BASE_URL'] ?? $_ENV['WEB_URL'] ?? null,
-			"iat" => time(), //"iat" (Issued At) Claim
-			"nbf" => time(), //"nbf" (Not Before) Claim
-		);
+        $payload = array_merge($_user, $payload);
+        unset($_user);
 
-		$_user = [
-			"name" => $user['name'] ?? null,
-			"user_id" => $user['user_id']
-		];
+        $jwt_secret = $_ENV['TRIBE_API_SECRET_KEY'] ?? $_ENV['DB_PASS'];
+        $jwt_token = JWT::encode($payload, $jwt_secret);
 
-		// "exp" (Expiration Time) Claim
-		if ($timeout) {
-			$payload["exp"] = time() + $timeout;
-		}
+        $_SESSION = $user;
 
-		$payload = array_merge($_user, $payload);
-		unset($_user);
+        return array(
+            "access_token" => $jwt_token,
+            "token_type" => "Bearer",
+            "user_id" => $user['user_id'],
+        );
+    }
 
-		$jwt_secret = $_ENV['TRIBE_API_SECRET_KEY'] ?? $_ENV['DB_PASS'];
-		$jwt_token = JWT::encode($payload, $jwt_secret);
+    public function getUserId(array $post)
+    {
+        $sql = new MySQL();
 
-		$_SESSION = $user;
+        $_user = false;
+        if ($post['email'] ?? false) {
+            $_user = $sql->executeSQL(
+                "SELECT id,content->>'$.password' as 'password'
+                FROM data
+                WHERE type='user' AND content->'$.email'='{$post['email']}'
+                ORDER BY id ASC LIMIT 1"
+            );
+        } else if ($post['mobile'] ?? false) {
+            $_user = $sql->executeSQL(
+                "SELECT id,content->>'$.password' as 'password'
+                FROM data
+                WHERE type = 'user' AND content->'$.mobile' = '{$post['mobile']}'
+                ORDER BY id ASC LIMIT 1"
+            );
+        } else if ($post['uname'] ?? false) {
+            $_user = $sql->executeSQL(
+                "SELECT id,content->>'$.password' as 'password'
+                FROM data
+                WHERE type = 'user' AND content-'$.uname' = '{$post['uname']}'
+                ORDER BY id ASC LIMIT 1"
+            );
+        }
 
-		return array(
-			"access_token" => $jwt_token,
-			"token_type" => "Bearer",
-			"user_id" => $user['user_id'],
-		);
-	}
+        if (!$_user) {
+            return false;
+        }
 
-	public function getUserId(array $post) {
-		$sql = new MySQL();
+        $_user = $_user[0];
 
-		$_user = false;
-		if ($post['email'] ?? false) {
-			$_user = $sql->executeSQL("SELECT id,content->>'$.password' as 'password' FROM data WHERE `type`='user' && content->'$.email'='{$post['email']}' AND content->>'$.password'!='' ORDER BY id ASC LIMIT 1");
-		} else if ($post['mobile'] ?? false) {
-			$_user = $sql->executeSQL("SELECT id,content->>'$.password' as 'password' FROM data WHERE `type`='user' && content->'$.mobile'='{$post['mobile']}' AND content->>'$.password'!='' ORDER BY id ASC LIMIT 1");
-		} else if ($post['uname'] ?? false) {
-			$_user = $sql->executeSQL("SELECT id,content->>'$.password' as 'password' FROM data WHERE `type`='user' && content->'$.uname'='{$post['uname']}' AND content->>'$.password'!='' ORDER BY id ASC LIMIT 1");
-		}
+        $is_valid = $this->verify_password($post['password'], $_user['password']);
 
-		if (!$_user) return false;
+        return $is_valid ? $_user['id'] : ['ok' => false];
+    }
 
-		$_user = $_user[0];
+    public function endSession()
+    {
+        $cookie_options = $this->cookie_options;
+        session_destroy();
 
-		$is_valid = $this->verify_password($post['password'], $_user['password']);
+        return setcookie('access_token', '', $cookie_options);
+    }
 
-		return $is_valid ? $_user['id'] : false;
-	}
+    public function secure_password(string $password)
+    {
+        return \password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+    }
 
-	public function endSession() {
-		$cookie_options = $this->cookie_options;
-		session_destroy();
+    public function verify_password(string $password, string $hash): bool
+    {
+        $is_valid = \password_verify($password, $hash);
 
-		return setcookie('access_token', '', $cookie_options);
-	}
+        if (!$is_valid) {
+            $is_valid = md5($password) == $hash;
+        }
 
-	public function secure_password (string $password)
-	{
-		return \password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
-	}
-
-	public function verify_password(string $password, string $hash): bool
-	{
-		$is_valid = \password_verify($password, $hash);
-
-		if (!$is_valid) {
-			$is_valid = md5($password) == $hash;
-		}
-
-		return $is_valid;
-	}
+        return $is_valid;
+    }
 }
